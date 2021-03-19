306ebac6e5868d21fe47d648df90c1edfcb7fb20
Patrick Corcoran <patrickcorcoran@Patricks-MacBook-Pro.local>
2021-03-18 20:52:40 -0500

    deploy 6

--- server.js
+++ server.js
@@ -153,6 +153,16 @@ app.post("/api/register", function (req, res) {
 //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 // });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 // Start the API server
 // ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
 db.sequelize.sync({ force: false }).then(() => {
