170de716635efb2fbb7a8b62ad9e530aed3aaaa6
Patrick Corcoran <patrickcorcoran@Patricks-MacBook-Pro.local>
2021-03-18 20:34:58 -0500

    commit to deploy 5

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: "us-cdbr-east-03.cleardb.com",
-    user: "b93be5449c4f41",
-    password: "7e36426f",
-    database: "heroku_e62224d990107c0",
-});
+// const db = mysql.createConnection({
+//     host: "us-cdbr-east-03.cleardb.com",
+//     user: "b93be5449c4f41",
+//     password: "7e36426f",
+//     database: "heroku_e62224d990107c0",
+// });
 
-module.exports = db;
+// module.exports = db;
 
 //mysql://b93be5449c4f41:7e36426f@us-cdbr-east-03.cleardb.com/heroku_e62224d990107c0?reconnect=true
